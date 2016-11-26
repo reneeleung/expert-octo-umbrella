32ff7daa3dc807e741057348fb52b04d58888657
Renee Leung <reneeleung@Renees-MacBook-Pro.local>
2016-11-26 15:48:15 -0500

    Initial Commit

---
+++ BIHackathon/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  BIHackathon
+//
+//  Created by Renee Leung on 2016-11-26.
+//  Copyright © 2016 Renee Leung. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
